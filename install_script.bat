e4ffed666eb334848aac47a27aa2519eb09739e0
Matt Oliver <protogonoi@gmail.com>
2017-04-19 23:27:59 +1000

    Improve install scripts detection of Visual Studio.

--- install_script.bat
+++ install_script.bat
@@ -1,4 +1,5 @@
-@ECHO OFF
+@echo OFF
+setlocal enabledelayedexpansion
 
 REM Defined cript variables
 set YASMDL=http://www.tortall.net/projects/yasm/releases
@@ -14,14 +15,14 @@ SET ERROR=0
 REM Check what architecture we are installing on
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
     echo Detected 64 bit system...
-    set SYSARCH=x64
+    set SYSARCH=64
 ) else if "%PROCESSOR_ARCHITECTURE%"=="x86" (
     if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
         echo Detected 64 bit system running 32 bit shell...
-        set SYSARCH=x64
+        set SYSARCH=64
     ) else (
         echo Detected 32 bit system...
-        set SYSARCH=x32
+        set SYSARCH=32
     )
 ) else (
     echo Error: Could not detect current platform architecture!"
@@ -51,7 +52,7 @@ REM First check for a environment variable to help locate the VS installation
 if defined VS140COMNTOOLS (
     if exist "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" (
         echo Visual Studio 2015 environment detected...
-        call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" 1>NUL 2>NUL
+        call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" >nul 2>&1
         set MSVC_VER=14
         goto MSVCVarsDone
     )
@@ -59,76 +60,69 @@ if defined VS140COMNTOOLS (
 if defined VS120COMNTOOLS (
     if exist "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" (
         echo Visual Studio 2013 environment detected...
-        call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" 1>NUL 2>NUL
+        call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" >nul 2>&1
         set MSVC_VER=12
         goto MSVCVarsDone
     )
 )
 
 REM Check for default install locations based on current system architecture
-if "%SYSARCH%"=="x32" (
-    goto MSVCVARSX86
-) else if "%SYSARCH%"=="x64" (
-    goto MSVCVARSX64
+if "%SYSARCH%"=="32" (
+    set MSVCVARSDIR=
+    set WOWNODE=
+) else if "%SYSARCH%"=="64" (
+    set MSVCVARSDIR=\amd64
+    set WOWNODE=\WOW6432Node
 ) else (
     goto Terminate
 )
 
-:MSVCVARSX86
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" (
+reg.exe query "HKLM\SOFTWARE%WOWNODE%\Microsoft\VisualStudio\SxS\VS7" /v 15.0 >nul 2>&1
+if not ERRORLEVEL 1 (
     echo Visual Studio 2017 installation detected...
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" 1>NUL 2>NUL
+    for /f "skip=2 tokens=2,*" %%a in ('reg.exe query "HKLM\SOFTWARE%WOWNODE%\Microsoft\VisualStudio\SxS\VS7" /v 15.0') do (set VSINSTALLDIR=%%b)
+    call "!VSINSTALLDIR!VC\Auxiliary\Build\vcvars%SYSARCH%.bat" >nul 2>&1
     set MSVC_VER=15
     goto MSVCVarsDone
-) else if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat" (
+)
+reg.exe query "HKLM\Software%WOWNODE%\Microsoft\VisualStudio\14.0" /v "InstallDir" >nul 2>&1
+if not ERRORLEVEL 1 (
     echo Visual Studio 2015 installation detected...
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat" 1>NUL 2>NUL
+    for /f "skip=2 tokens=2,*" %%a in ('reg.exe query "HKLM\Software%WOWNODE%\Microsoft\VisualStudio\14.0" /v "InstallDir"') do (set VSINSTALLDIR=%%b)
+    call "!VSINSTALLDIR!\VC\bin%MSVCVARSDIR%\vcvars%SYSARCH%.bat" >nul 2>&1
     set MSVC_VER=14
     goto MSVCVarsDone
-) else if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat" (
-    echo Visual Studio 2013 installation detected...
-    call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat" 1>NUL 2>NUL
-    set MSVC_VER=12
-    goto MSVCVarsDone
-) else (
-    echo Error: Could not find valid 64 bit x86 Visual Studio installation!
-    goto Terminate
 )
-
-:MSVCVARSX64
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
-    echo Visual Studio 2017 installation detected...
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" 1>NUL 2>NUL
-    set MSVC_VER=15
-    goto MSVCVarsDone
-) else if exist C:\"Program Files (x86)\Microsoft Visual Studio 14.0"\VC\bin\amd64\vcvars64.bat (
-    echo Visual Studio 2015 installation detected...
-    call C:\"Program Files (x86)\Microsoft Visual Studio 14.0"\VC\bin\amd64\vcvars64.bat 1>NUL 2>NUL
-    set MSVC_VER=14
-    goto MSVCVarsDone
-) else if exist C:\"Program Files (x86)\Microsoft Visual Studio 12.0"\VC\bin\amd64\vcvars64.bat (
+reg.exe query "HKLM\Software%WOWNODE%\Microsoft\VisualStudio\13.0" /v "InstallDir" >nul 2>&1
+if not ERRORLEVEL 1 (
     echo Visual Studio 2013 installation detected...
-    call C:\"Program Files (x86)\Microsoft Visual Studio 12.0"\VC\bin\amd64\vcvars64.bat 1>NUL 2>NUL
-    set MSVC_VER=12
+    for /f "skip=2 tokens=2,*" %%a in ('reg.exe query "HKLM\Software%WOWNODE%\Microsoft\VisualStudio\13.0" /v "InstallDir"') do (set VSINSTALLDIR=%%b)
+    call "!VSINSTALLDIR!\VC\bin%MSVCVARSDIR%\vcvars%SYSARCH%.bat" >nul 2>&1
+    set MSVC_VER=13
     goto MSVCVarsDone
-) else (
-    echo Error: Could not find valid 64 bit x86 Visual Studio installation!
-    goto Terminate
 )
+echo Error: Could not find valid Visual Studio installation!
+goto Terminate
 
 :MSVCVarsDone
 REM Get the location of the current msbuild
-powershell.exe -Command ((Get-Command msbuild.exe).Path ^| Split-Path -parent) > msbuild.txt
-set /p MSBUILDDIR=<msbuild.txt
-del /F /Q msbuild.txt 1>NUL 2>NUL
-if "%MSBUILDDIR%"=="" (
+powershell.exe -Command ((Get-Command msbuild.exe)[0].Path ^| Split-Path -parent) > msbuild.txt
+findstr /C:"Get-Command" msbuild.txt >nul 2>&1
+if not ERRORLEVEL 1 (
     echo Error: Failed to get location of msbuild!
+    del /F /Q msbuild.txt >nul 2>&1
     goto Terminate
 )
+set /p MSBUILDDIR=<msbuild.txt
+del /F /Q msbuild.txt >nul 2>&1
 if "%MSVC_VER%"=="15" (
     set VCTargetsPath="..\..\..\Common7\IDE\VC\VCTargets"
 ) else (
-    set VCTargetsPath="..\..\..\Microsoft.Cpp\v4.0\V%MSVC_VER%0"
+    if "%MSBUILDDIR%"=="%MSBUILDDIR:amd64=%" (
+        set VCTargetsPath="..\..\Microsoft.Cpp\v4.0\V%MSVC_VER%0"
+    ) else (
+        set VCTargetsPath="..\..\..\Microsoft.Cpp\v4.0\V%MSVC_VER%0"
+    )
 )
 
 REM Convert the relative targets path to an absolute one
@@ -145,8 +139,8 @@ if not "%CURRDIR%"=="%CD%" (
 
 REM copy the BuildCustomizations to VCTargets folder
 echo Installing build customisations...
-del /F /Q "%VCTargetsPath%\BuildCustomizations\yasm.*" 1>NUL 2>NUL
-copy /B /Y /V "%SCRIPTDIR%\yasm.*" "%VCTargetsPath%\BuildCustomizations\" 1>NUL 2>NUL
+del /F /Q "%VCTargetsPath%\BuildCustomizations\yasm.*" >nul 2>&1
+copy /B /Y /V "%SCRIPTDIR%\yasm.*" "%VCTargetsPath%\BuildCustomizations\" >nul 2>&1
 if not exist "%VCTargetsPath%\BuildCustomizations\yasm.props" (
     echo Error: Failed to copy build customisations!
     echo    Ensure that this script is run in a shell with the necessary write privileges
@@ -159,23 +153,20 @@ if not exist "%SCRIPTDIR%\yasm\" (
     call :DownloadYasm
 ) else (
     REM Use the bundled binaries
-    if "%SYSARCH%"=="x32" (
-        copy /B /Y /V "%SCRIPTDIR%\yasm\yasm-32.exe" "%SCRIPTDIR%\yasm.exe" 1>NUL 2>NUL
-    ) else if "%SYSARCH%"=="x64" (
-        copy /B /Y /V "%SCRIPTDIR%\yasm\yasm-64.exe" "%SCRIPTDIR%\yasm.exe" 1>NUL 2>NUL
-    ) else (
-        goto Terminate
-    )
+    copy /B /Y /V "%SCRIPTDIR%\yasm\yasm-%SYSARCH%.exe" "%SCRIPTDIR%\yasm.exe" >nul 2>&1
 )
 
 REM copy yasm executable to VC installation folder
 echo Installing required YASM release binary...
-del /F /Q "%VCINSTALLDIR%\yasm.exe" 1>NUL 2>NUL
-move /Y "%SCRIPTDIR%\yasm.exe" "%VCINSTALLDIR%\" 1>NUL 2>NUL
-if not exist "%VCINSTALLDIR%\yasm.exe" (
+del /F /Q "%VCINSTALLDIR%\yasm.exe" >nul 2>&1
+move /Y "%SCRIPTDIR%\yasm.exe" "%VCINSTALLDIR%\" >nul 2>&1
+set INSTALLED=1
+if exist "%SCRIPTDIR%\yasm.exe" set INSTALLED=0
+if not exist "%VCINSTALLDIR%\yasm.exe" set INSTALLED=0
+if %INSTALLED% equ 0 (
     echo Error: Failed to install YASM binary!
     echo    Ensure that this script is run in a shell with the necessary write privileges
-    del /F /Q "%SCRIPTDIR%\yasm.exe" 1>NUL 2>NUL
+    del /F /Q "%SCRIPTDIR%\yasm.exe"  >nul 2>&1
     goto Terminate
 )
 echo Finished Successfully
@@ -192,15 +183,9 @@ IF "%APPVEYOR%"=="" (
 exit /b %ERROR%
 
 :DownloadYasm
-if "%SYSARCH%"=="x32" (
-    set YASMDOWNLOAD=%YASMDL%/yasm-%YASMVERSION%-win32.exe
-) else if "%SYSARCH%"=="x64" (
-    set YASMDOWNLOAD=%YASMDL%/yasm-%YASMVERSION%-win64.exe
-) else (
-    goto Terminate
-)
 echo Downloading required YASM release binary...
-powershell.exe -Command (New-Object Net.WebClient).DownloadFile('%YASMDOWNLOAD%', '%SCRIPTDIR%\yasm.exe') 1>NUL 2>NUL
+set YASMDOWNLOAD=%YASMDL%/yasm-%YASMVERSION%-win%SYSARCH%.exe
+powershell.exe -Command (New-Object Net.WebClient).DownloadFile('%YASMDOWNLOAD%', '%SCRIPTDIR%\yasm.exe') >nul 2>&1
 if not exist "%SCRIPTDIR%\yasm.exe" (
     echo Error: Failed to download required YASM binary!
     echo    The following link could not be resolved "%YASMDOWNLOAD%"
